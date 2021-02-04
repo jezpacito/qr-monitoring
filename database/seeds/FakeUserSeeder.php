88e69021cd994b6709d8dce8f61142b714df29b1
gatmaitanjezreel <gatmaitanjezreel@gmail.com>
2021-02-05 00:09:08 +0800

    changes users factory error

--- database/seeds/FakeUserSeeder.php
+++ database/seeds/FakeUserSeeder.php
@@ -12,16 +12,28 @@ class FakeUserSeeder extends Seeder
      */
     public function run()
     {
-        $users =factory(User::class, 5)->create();
 
-        $admin =factory(User::class)->create([
-            'email' => 'admin@gmail.com'
+        $user= \App\User::create([
+            'gender' =>1,
+            'contact_no' =>'0977452221',
+            'address' =>'General Santos City',
+            'lname' =>'Santos',
+            'fname'=>'Juan',
+            'email' => 'admin@gmail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
-        $admin->assignRole('admin');
+        $user->assignRole('admin');
 
-        foreach ($users as $user){
-            $user->assignRole('admin');
-        }
+//        $users =factory(User::class, 5)->create();
+//        $admin =factory(User::class)->create([
+//            'email' => 'admin@gmail.com',
+//            'password' => \Illuminate\Support\Facades\Hash::make('12345678')
+//        ]);
+//        $admin->assignRole('admin');
+
+//        foreach ($users as $user){
+//            $user->assignRole('admin');
+//        }
 
     }
 }
--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -14,5 +14,16 @@ class RoleSeeder extends Seeder
     public function run()
     {
      Role::create(['name' =>'admin']);
+
+        $user= \App\User::create([
+            'gender' =>1,
+            'contact_no' =>'0977452221',
+            'address' =>'General Santos City',
+            'lname' =>'Santos',
+            'fname'=>'Juan',
+            'email' => 'admin@gmail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+        $user->assignRole('admin');
     }
 }
