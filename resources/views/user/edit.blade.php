f0031ab3b3025e4bfb1f07158dc4cf9e4a43979d
gatmaitanjezreel <gatmaitanjezreel@gmail.com>
2021-02-22 23:43:36 +0800

    edit ui fixed user profile update

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,6 +1,20 @@
 @extends('layouts.table')
 
 @section('content')
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+        <meta name="author" content="Creative Tim">
+        <link rel="icon" href="{{asset('assets/img/brand/favicon.png')}}" type="image/png">
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+        <!-- Icons -->
+        <link rel="stylesheet" href="{{asset('assets/vendor/nucleo/css/nucleo.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css')}}" type="text/css">
+        <!-- Argon CSS -->
+        <link rel="stylesheet" href="{{asset('assets/css/argon.css?v=1.2.0')}}" type="text/css">
+    </head>
     <div class="container-fluid mt--6">
         <div class="row">
             <div class="col-xl-4 order-xl-2">
